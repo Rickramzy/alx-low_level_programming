fe070d926b67cf88d02c559c5f1f048ed8d127b5
rickramzy <oguejioforrapheal@rocketmail.com>
2023-02-27 19:03:07 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * void swap_int(int *a, int *b) - Function that swaps 
+ * value of intergers
+ * @a: intiger for swap
+ * @b:  intiger for swap
+ */
+{
+	int m;
+	
+	m = *a;
+	*a = *b;
+	*b = a
+
+}
