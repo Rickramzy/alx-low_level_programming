4e4e73594452873f0f09edb569e482bf9db8fb3d
rickramzy <oguejioforrapheal@rocketmail.com>
2023-04-05 04:23:16 -0700

    0-print_list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,24 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nodes = 0;
 
-	/*return 0 as no of nodes when h is null*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
-
-		/*go to the next node*/
 		h = h->next;
-		/*count no of nodes*/
-		num++;
 	}
 
-	/*print last node*/
-	printf("%d\n", h->n);
-
-	return (num);
+	return (nodes);
 }
