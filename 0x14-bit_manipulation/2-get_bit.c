f0053a0fc1cfc4127332fff65796094451ce3c23
rickramzy <oguejioforrapheal@rocketmail.com>
2023-04-14 11:13:10 -0700

    2.10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,11 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bit, bin;
 
 	if (index > (sizeof(unsigned long int) * 8))
-			return (-1);
-	
+		return (-1);
+
 	bin = n >> index;
-	
 	bit = bin & 1;
-	
 	return (bit);
 }
