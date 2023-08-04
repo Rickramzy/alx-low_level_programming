05247ae109d322de6a78b7c4e38b8a926b205372
Rickramzy <oguejioforrapheal@rocketmail.com>
2023-08-03 19:14:06 -0700

    4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
-/** clear_bit - sets the laue to a given index
+/**
+ * clear_bit - sets the laue to a given index.
  * @n: parameter
  * @index: index
+ * Desc: a function that sets the value of a bit to 0 at a given index.
+ *
  * Return: 1 1f it works, -1 id error
  */
 int clear_bit(unsigned long int *n, unsigned int index)
