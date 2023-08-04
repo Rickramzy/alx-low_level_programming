fcf46ce3c99e4ccc040cdaab616aa36a53c37c85
Rickramzy <oguejioforrapheal@rocketmail.com>
2023-08-03 18:41:01 -0700

    4. 100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** clear_bit - sets the laue to a given index
+ * @n: parameter
+ * @index: index
+ * Return: 1 1f it works, -1 id error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
