7bbf1b062c1c3ac46419f696f238d31700000b61
Rickramzy <oguejioforrapheal@rocketmail.com>
2023-08-07 15:35:33 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,27 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "main.h"
 
 /**
  * read_textfile - Read text file and print to STDOUT
  * @filename: the text file being read
  * @letters: number of bytes read and printed
- * Return: w- actual no. of bytes read and printed
- * 	0 when function fails or filenam is NULL.
+ * Return: If the function failes or filename is NULL - 0.
+ * Otgherwise - the actual number of the bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf:
-	ssize_t fd:
-	ssize_t w:
-	ssize_t t:
+	int fd;
+	char *buff;
+	ssize_t bytes, r;
 
-	fd =open(filename. o_RDONLY);
-	if (fd == -1)
+	if (!filename)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		close(fd);
+		return (0);
+	}
 
-	free(buf);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes = read(fd, buff, letters);
+
+	if (bytes == -1)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
+
+	r = write(STDOUT_FILENO, buff, bytes);
+
+	if (r == -1)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
+	
 	close(fd);
-	return (w);
+	return (bytes);
 }
